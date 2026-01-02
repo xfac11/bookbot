f359e7c9d2b1957b2e33221f3b58b4cc664490fd
xfac11 <xfac115@gmail.com>
2026-01-02 12:23:50 +0100

    Added the main function and a get_book_text function that returns the content of the book.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(file_path):
+    contents = None
+
+    with open(file_path) as file:
+        contents = file.read()
+
+    return contents
+
+def main():
+    file_path = "books/frankenstein.txt"
+    contents = get_book_text(file_path)
+    print(contents)
+
+
+main()
