6c5429cef677ea04046e0521b7101c5263b90117
xfac11 <xfac115@gmail.com>
2026-01-02 12:34:36 +0100

    Added a get_word_count function that works with the split() method

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ def get_book_text(file_path):
 
     return contents
 
+def get_word_count(text):
+    count = 0
+    if len(text) == 0:
+        raise Exception("text is an empty string")
+    list = text.split()
+    count = len(list)
+    return count
+
 def main():
     file_path = "books/frankenstein.txt"
     contents = get_book_text(file_path)
-    print(contents)
+    word_count = get_word_count(contents)
+    print(f"Found {word_count} total words")
 
 
 main()
